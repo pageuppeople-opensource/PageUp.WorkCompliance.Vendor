1ddad3319ada69c3cc3b647dafe1a174e088a299
Scott Mcfarlane <scottm@pageuppeople.com>
2018-02-23 17:04:13 +1100

    Updates routes to use api-gateway

--- PageUp.Compliance.Vendor/Core/ComplianceInvitation.cs
+++ PageUp.Compliance.Vendor/Core/ComplianceInvitation.cs
@@ -7,7 +7,7 @@ namespace PageUp.Compliance.Vendor.Core
 {
     public class ComplianceInvitation
     {
-        public string RequestId { get; set; }
+        public string InvitationId { get; set; }
         public DateTime Created { get; set; }
         public DateTime Modified { get; set; }
         public string FirstName { get; set; }
@@ -15,10 +15,22 @@ namespace PageUp.Compliance.Vendor.Core
         public string Email { get; set; }
         public string LocationCode { get; set; }
         public string PositionCode { get; set; }
+        public Site Site  {get; set;}
         public string EmployeeId { get; set; }
-        public string IsPending { get; set; }
-        public ComplianceStatus[] Statuses { get; set; }
         public int? OnboardingId { get; set; }
         public string ExternalReferenceId { get; set; }
+        public string IsPending { get; set; }
+        public ComplianceStatus[] Statuses { get; set; }
+
+    }
+
+    public class Site
+    {
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string PostCode { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string SiteCode { get; set; }
     }
 }
